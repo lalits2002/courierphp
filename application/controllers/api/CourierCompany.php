3514503e297a6a9d67b518443b7c4244675a44d7
lalits2002 <lalits2002@gmail.com>
2018-11-03 11:14:37 -0700

    Initial commit

---
+++ application/controllers/api/CourierCompany.php
@@ -0,0 +1,108 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CourierCompany extends BD_Controller {
+
+    function __construct() {
+        // Construct the parent class
+        parent::__construct();
+//        $this->auth();
+        $this->load->library('validation');
+        $this->load->model('CourierCompanyModel');
+    }
+
+    public function test_post() {
+
+        $theCredential = $this->user_data;
+        $this->response($theCredential, 200); // OK (200) being the HTTP response code
+    }
+
+    public function users_post() {
+        $params = $this->input->post();
+        $this->validation->set_post($params);
+
+        $this->validation->required(array('FIRSTNAME', 'PHONENO', 'EMAIL', 'LOGINPWD'), 'Fields are required')
+                ->email('EMAIL', 'Entered email is not valid')
+                ->minlen('PHONENO', 10, 'Invalid length of phone')
+                ->callback('_checkDuplicateMail', 'Phone or Email already exists.', array('EMAIL', 'PHONENO'))
+//		->callback(array($your_model, 'method_name'), 'Error message', array('parameter1', 'parameter2', 'parameter3'))
+//		->callback('_other_func', 'Your error.')
+        ;
+
+        if ($this->validation->is_valid()) {
+            $result = $this->M_main->createUser($params);
+        } else {
+            $result = $this->validation->get_error_message();
+            exit;
+        }
+        return $result;
+    }
+
+    public function _checkDuplicate($email, $phone) {
+        $email = $this->validation->get_data($email);
+        $phone = $this->validation->get_data($phone);
+        $result = $this->M_main->getCompany($email, $phone);
+
+        if (count($result) > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public function company_get(){
+        
+    }
+    
+    public function company_post(){
+        $params = $this->input->post();
+        $this->validation->set_post($params);
+        
+         $this->validation->required(array('BUSINESSNAME', 'CONTACTPERSON', 'PHONENO', 'LOGINPWD'), 'Fields are required')
+                ->email('EMAIL', 'Entered email is not valid')
+                ->minlen('PHONENO', 10, 'Invalid length of phone')
+                ->callback('_checkDuplicate', 'Phone or Email already exists.', array('EMAIL', 'PHONENO'));
+    }
+    
+
+    public function users_get($email = '', $phone = '') {
+        // Users from a data store e.g. database
+        try {
+            $result = array();
+            $result = $this->M_main->getUser($email, $phone);
+
+            if (is_array($result) && count($result) > 0) {
+                return $this->response(['status' => true, 'data' => $result, 'message' => 'success'], REST_Controller::HTTP_OK);
+            } else {
+                return $this->response([
+                            'status' => FALSE,
+                            'message' => 'No users were found'
+                                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+            }
+        } catch (Exception $e) {
+            echo '<pre>';
+            print_r($e);
+            exit;
+        }
+    }
+
+    public function users_delete() {
+        $id = (int) $this->get('id');
+
+        // Validate the id.
+        if ($id <= 0) {
+            // Set the response and exit
+            $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        }
+
+        // $this->some_model->delete_something($id);
+        $message = [
+            'id' => $id,
+            'message' => 'Deleted the resource'
+        ];
+
+        $this->set_response($message, REST_Controller::HTTP_NO_CONTENT); // NO_CONTENT (204) being the HTTP response code
+    }
+
+}
---
+++ application/models/m_main.php
@@ -0,0 +1,66 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script allowed');
+
+class M_main extends CI_Model {
+
+    function get_user($q) {
+        return $this->db->get_where('m_user', $q);
+    }
+
+    function getUser($mail, $phone) {
+        try{
+             $sql = "call hand2handservices.GETUSERDETAILS(@v_whereclause := 'PHONENO = $phone OR EMAIL = ''" . $mail . "''');";
+        $query = $this->db->query($sql);
+        $result = $query->result_array();
+//        echo '<pre>'; print_r($result);exit;
+        if (count($result) > 0) {
+            return $result;
+        } else {
+            return false;
+        }
+        }catch(Exception $e){
+            echo '<pre>'; print_r($e);exit;
+        }
+       
+    }
+
+    function createUser($params) {
+//        print_r($params);exit;
+//        $sql = "CALL hand2handservices.ADDUSER(?,?,?,?,?,?,?,?,?, ?, ?,?,?)";
+//        $sql = "CALL hand2handservices.ADDUSER(@v_firstname:=?, @v_lastname:=?,@v_phoneno:=?,@v_phonenoverified:=?,@v_email:=?,@v_emailverified:=?,@v_loginpwd:=?,@v_acceptedtermsconditions:=?,@v_usertype:=?, @v_referencecode:=?, @v_gstn:=?,@v_couriercompanyid:=?,@v_output_userid);";
+
+
+
+        $firstName = $params['FIRSTNAME'];
+        $lastName = $params['LASTNAME'];
+        $phoneNumber = $params['PHONENO'];
+        $phoneVerified = $params['PHONENOVERIFIED'];
+        $email = $params['EMAIL'];
+        $emailVerified = $params['EMAILVERIFIED'];
+        $loginPass = $params['LOGINPWD'];
+        $acceptTerms = $params['ACCEPTEDTERMSCONDITIONS'];
+        $userTypeId = $params['USERTYPEID'];
+        $referenceCode = $params['REFERENCECODE'];
+        $gstn = $params['GSTN'];
+
+        $sql = "CALL hand2handservices.ADDUSER('" . $firstName . "', '" . $lastName . "','" . $phoneNumber . "','" . $phoneVerified . "','" . $email . "','" . $emailVerified . "','" . $loginPass . "','" . $acceptTerms . "', '" . $userTypeId . "', '" . $referenceCode . "','" . $gstn . "',@v_couriercompanyid:=null,@v_output_userid);";
+
+//            $sql = "CALL hand2handservices.ADDUSER('".$this->db->escape($firstName)."', '".$this->db->escape($lastName)."','".$this->db->escape($phoneNumber)."','".$this->db->escape($phoneVerified)."','".$this->db->escape($email)."','".$this->db->escape($emailVerified)."','".$this->db->escape($loginPass)."','".$this->db->escape($acceptTerms)."', '".$this->db->escape($userTypeId)."', '".$this->db->escape($referenceCode)."','".$this->db->escape($gstn)."',@v_couriercompanyid:=null,@v_output_userid);";
+//          $query = $this->db->query($sql,array($firstName , $lastName , $phoneNumber, $phoneVerified  , $email, $emailVerified  , $loginPass, $acceptTerms, $userTypeId, $referenceCode, $gstn ));
+//          $query = $this->db->query($sql,array($firstName , $lastName , $phoneNumber, $phoneVerified  , $email, $emailVerified  , $loginPass, $acceptTerms, $userTypeId, $referenceCode, $gstn ));
+        $query1 = $this->db->query($sql);
+        $userID = $this->db->query("Select @v_output_userid as userid");
+        $result = $userID->result_array();
+        if (isset($result[0]['userid']) && $result[0]['userid'] == 0) {
+            return array('status' => 0, 'userMsg' => 'Unknown error from database.');
+        } else {
+            return array('status' => 200, 'userMsg' => '', 'data' => $result);
+        }
+
+
+//            return $query->result_array(); 
+    }
+
+}
